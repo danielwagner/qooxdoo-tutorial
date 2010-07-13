120c9f085540a45fa94bb1706ab56ee3682df7fc
Martin Wittemann <mw@mac-mini-wt.local>
2010-07-13 16:22:03 +0200

    Change the posting and loading of tweets due to a change in the twitter API.

--- source/class/twitter/TwitterService.js
+++ source/class/twitter/TwitterService.js
@@ -22,7 +22,7 @@ qx.Class.define("twitter.TwitterService",
     
     fetchTweets : function() {
       if (this.__store == null) {
-        var url = "http://twitter.com/statuses/friends_timeline.json";
+        var url = "http://api.twitter.com/1/statuses/public_timeline.json";
         this.__store = new qx.data.store.Jsonp(url, null, "callback");        
         this.__store.bind("model", this, "tweets");
       } else {
@@ -33,31 +33,9 @@ qx.Class.define("twitter.TwitterService",
     
     post : function(message) 
     {
-      // get the username and password
-      var username = prompt("Username");
-      var password = prompt("Password");
-      
-      var query = "use 'http://www.yqlblog.net/samples/twitter.status.xml';" + 
-                  "insert into twitter.status (status,username,password)" + 
-                  "values ('" + message + "', '" + username + "', '" + password + "')";
-
-      var store = new qx.data.store.Yql(query, null, true);
-      store.addListener("loaded", function(e) {
-        var model = e.getData();
-        try {
-          var returnedText = model.getQuery().getResults().getStatus().getText();
-          // seems like everything is ok
-          if (returnedText == message) {
-            this.fireEvent("postOk");
-          }
-        } catch (e) {
-          try {
-            alert(model.getError().getDescription());
-          } catch (e) {
-            alert("Could not post."); 
-          }
-        }
-      }, this);
+      // redirect to twitter because an OAuth login would be too much for this 
+      // tutorial
+      window.open("http://twitter.com/?status=" + encodeURIComponent(message));
     }
   }
 });
