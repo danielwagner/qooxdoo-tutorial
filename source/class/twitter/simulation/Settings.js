339fb16ebacf401ceecd4bc209f745ad924d2c5f
Daniel Wagner <dwagner@dwagner.(none)>
2011-09-13 15:41:55 +0200

    Commented and formatted for better readability

--- source/class/twitter/simulation/Settings.js
+++ source/class/twitter/simulation/Settings.js
@@ -7,16 +7,29 @@ qx.Class.define("twitter.simulation.Settings", {
     testChangeLanguage : function()
     {
       this.getQxSelenium().setSpeed(1000);
-      // Open the Preferences window
-      this.getQxSelenium().qxClick("qxhv=*/[@label=Preferences]");
-      this.assertTrue(this.getQxSelenium().isElementPresent("qxhv=[@classname=twitter.SettingsWindow]"));
-      // Click the radio button for Romanian and close the window
-      this.getQxSelenium().qxClick("qxhv=[@classname=twitter.SettingsWindow]/*/[@label=Romanian]");
-      this.getQxSelenium().qxClick("qxhv=[@classname=twitter.SettingsWindow]/qx.ui.container.Composite/[@icon=close\.gif]");
-      this.assertFalse(this.getQxSelenium().isElementPresent("qxhv=[@classname=twitter.SettingsWindow]"));
+      
+      // Click the Preferences button
+      var preferencesButtonLocator = "qxhv=*/[@label=Preferences]";
+      this.getQxSelenium().qxClick(preferencesButtonLocator);
+      // Check if the Preferences window opened
+      var settingsWindowLocator = "qxhv=[@classname=twitter.SettingsWindow]";
+      var settingsWindowPresent = this.getQxSelenium().isElementPresent(settingsWindowLocator);
+      this.assertTrue(settingsWindowPresent);
+      
+      // Click the radio button for Romanian
+      var romanianLabelLocator = "qxhv=[@classname=twitter.SettingsWindow]/*/[@label=Romanian]";
+      this.getQxSelenium().qxClick(romanianLabelLocator);
+      // Click the window's close button
+      var windowCloseButtonLocator = "qxhv=[@classname=twitter.SettingsWindow]/qx.ui.container.Composite/[@icon=close\.gif]";
+      this.getQxSelenium().qxClick(windowCloseButtonLocator);
+      // Check if the window was closed
+      settingsWindowPresent = this.getQxSelenium().isElementPresent(settingsWindowLocator);
+      this.assertFalse(settingsWindowPresent);
+      
       // Get the translated string for the Preferences button label 
-      var translatedLabel = this.getQxSelenium().getRunInContext("qxhv=[@classname=twitter.MainWindow]/*/[@label=Preferences]",
+      var translatedLabel = this.getQxSelenium().getRunInContext(preferencesButtonLocator,
       "return this.getLabel().translate().toString()");
+      // Check if the label was translated
       this.assertEquals("Preferinte", translatedLabel);
     }
   }
